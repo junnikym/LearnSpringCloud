0b4526dc3c7f9e7e339aaaf44034fc10774462af
junnikym <junnikym@gmail.com>
2022-01-16 16:39:59 +0900

    initialize spring boot project

---
+++ src/main/java/name/junnikym/learn_spring_cloud/LearnSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package name.junnikym.learn_spring_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/name/junnikym/learn_spring_cloud/LearnSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package name.junnikym.learn_spring_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
